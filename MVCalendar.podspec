c3633d648fd963ef4168e37b99760c788498807c
Mohan Reddy <mohanvydu@gmail.com>
2016-11-02 08:41:32 +0530

    Fixes in travis build procedure

--- MVCalendar.podspec
+++ MVCalendar.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'MVCalendar'
-  s.version          = '0.1.7'
+  s.version          = '0.1.8'
   s.summary          = 'MVCalendar is a custom calendar in swift.'
 
 
--- MVCalendar/Classes/MVCalendarView.swift
+++ MVCalendar/Classes/MVCalendarView.swift
@@ -57,7 +57,7 @@ public class MVCalendarView: UIView, UICollectionViewDataSource, UICollectionVie
     lazy var calendarView : UICollectionView = {
         
         let layout = MVCalendarFlowLayout()
-        layout.scrollDirection =
+        layout.scrollDirection = .horizontal
         layout.minimumLineSpacing = 0
         layout.minimumInteritemSpacing = 0
         
